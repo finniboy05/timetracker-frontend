16cd57ca2bb26eced58c4c830c3679aecfbf4e35
Finn Kuhni <finn.kuhni@bbzbl-it.ch>
2023-05-26 08:27:02 +0200

    add security

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TimetrackerComponent } from './timetracker/timetracker.component';
+import { AuthConfig, OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TimetrackerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    OAuthModule.forRoot({resourceServer: {sendAccessToken: true}}),
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/ILV',
+  requireHttps: false,
+  redirectUri: environment.frontendBaseUrl,
+  postLogoutRedirectUri: environment.frontendBaseUrl,
+  clientId: 'demoapp',
+  scope: 'openid profile roles offline_access',
+  responseType: 'code',
+  showDebugInformation: true,
+  requestAccessToken: true,
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshTimeout: 500,
+  clearHashAfterLogin: true,
+};
+
+export function storageFactory(): OAuthStorage {
+  return sessionStorage;
+}
